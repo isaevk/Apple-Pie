4c0b1ec5f09b5ca5bdf707ec764df170e12cfcfa
Kirill Isaev <thisiskir.dev@ya.ru>
2022-08-03 18:39:22 +0300

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Kirill Dev on 03.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
