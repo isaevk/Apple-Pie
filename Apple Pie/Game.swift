6c4875ac182a818b0e24695e4ee9889453bbe4c6
Kirill Isaev <thisiskir.dev@ya.ru>
2022-08-05 20:21:04 +0300

    Added Game model

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Kirill Dev on 05.08.2022.
+//
+
+
+struct Game{
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,11 +9,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var coorectWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: - Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfWords = [
+        "Tokyo",
+        "Delhi",
+        "Shanghai",
+        "São Paulo",
+        "Mexico City",
+        "Cairo",
+        "Mumbai",
+        "Beijing",
+        "Dhaka",
+        "Osaka",
+        "New York",
+        "Karachi",
+        "Buenos Aires",
+        "Chongqing",
+        "Istanbul",
+        "Calcutta",
+        "Manila",
+        "Lagos",
+        "Rio de Janeiro",
+        "Tianjin",
+        "Kinshasa",
+        "Guangzhou",
+        "Los Angeles",
+        "Moscow",
+        "Shenzhen",
+        "Lahore",
+        "Bangalore",
+        "Paris",
+        "Bogota",
+        "Jakarta",
+        "Chennai",
+        "Lima",
+        "Bangkok",
+        "Seoul",
+        "Nagoya",
+        "Hyderabad",
+        "London",
+        "Tehran",
+        "Chicago",
+        "Chengdu",
+        "Nanjing",
+        "Wuhan",
+        "Ho Chi Minh City",
+        "Luanda",
+        "Ahmedabad",
+        "Kuala Lumpur",
+        "Xi'an",
+        "Hong Kong",
+        "Dongguan",
+        "Hangzhou",
+        "Foshan",
+        "Shenyang",
+        "Riyadh",
+        "Baghdad",
+        "Santiago",
+        "Surat",
+        "Madrid",
+        "Suzhou",
+        "Pune",
+        "Harbin",
+        "Houston",
+        "Dallas",
+        "Toronto",
+        "Dar es Salaam",
+        "Miami",
+        "Belo Horizonte",
+        "Singapore",
+        "Philadelphia",
+        "Atlanta",
+        "Fukuoka",
+        "Khartoum",
+        "Barcelona",
+        "Johannesburg",
+        "St. Petersburg",
+        "Qingdao",
+        "Dalian",
+        "Washington",
+        "Yangon",
+        "Alexandria",
+        "Jinan",
+    ]
+    let totalwins =  0
+    let totalLosses = 0
+    // MARK: - Mathods
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI(){
+        let movesRemaining = currentGame.incorrectMovesRemaining
+        let image = "Tree\(movesRemaining < 8 ? movesRemaining: 7)"
+        treeImageView.image = UIImage(named: image)
+        scoreLabel.text = "Выигрыши:\(totalwins), проигыши:\(totalLosses)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
 
+    // MARK: - IB Actions
 
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
